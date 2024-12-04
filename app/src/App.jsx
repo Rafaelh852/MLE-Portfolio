39d7208833de953ccf1cce6553e88baf3e1002b1
Rafael Hernandez <Rafael.Hernandez852@gmail.com>
2024-12-03 22:09:51 -0800

    boilerplate part2

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,23 @@
 import "./output.css"
+import TopBar from "./components/topbar"
+import Hero from "./components/hero"
+import About from "./components/about"
+import Footer from "./components/footer"
 
 function App(){
 
-    return (<div className="">
-        Hello World! 2
-    </div>)
+    return (   
+        <>
+        <TopBar/>
+        <div class="relative isolate px-6 pt-14 lg:px-8">
+            <Hero/>
+            <About/>
+            <Footer/>
+        </div>
+        </>
+        
+    
+)
 }
 
 export default App
---
+++ app/src/components/about.jsx
@@ -0,0 +1,25 @@
+function About(){
+    return(
+    <div class="bg-white py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+            <p class="text-base font-semibold leading-7 text-indigo-600">About</p>
+            <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Machine Learning Engineer</h1>
+            <div class="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+            <div>
+                <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+                <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas.</p>
+            </div>
+            
+            </div>
+            <div class="mt-10 flex">
+            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+            </div>
+        </div>
+        </div>
+    </div>
+  
+  )
+}
+
+export default About
